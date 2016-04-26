bdf8bee8d799d83e629a3564706db473424b0863
jhon edison <jhon_edinson11@hotmail.com>
2016-04-25 21:56:49 -0500

    create functions to init session and create an user

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
 import json
+from django.contrib.auth.models import User
 
 class IndexView(TemplateView):
     template_name = 'home/index.html'
@@ -13,6 +14,85 @@ class IndexView(TemplateView):
 class SignUpView(TemplateView):
     template_name = "home/sign-up-form.html"
     form_class = ""
+
+    def get(self, request, *args, **kwargs):
+
+        if "email" in request.GET:
+            #try:
+
+            response_data = {}
+            message = ""
+            exist = False
+            is_error = False
+
+            email = request.GET['email']
+            user = User.objects.filter(email = email)
+
+            if user:
+
+                message = "ya existe una cuenta con este email"
+                exist = True
+
+            else:
+
+                message = "no hay una cuenta con este email, por favor registrelo"
+                exist = False
+
+            """except Exception as e:
+
+                is_error = True
+                message = "error en el sistema por favor comuniquese con soporte"
+                response_data['type_error'] = type(e).__name__"""
+
+            response_data['exist'] = exist
+            response_data['message'] = message
+            response_data['is_error'] = is_error
+
+            response_json = json.dumps(response_data)
+            content_type = 'application/json'
+            return HttpResponse(response_json, content_type)
+
+        else:
+
+            dic = {}
+            context_instance = RequestContext(request)
+            template = self.template_name
+            return render_to_response(template, dic,context_instance)
+
+    def post(self, request, *args, **kwargs):
+
+        try:
+
+            response_data = {}
+            message = ""
+            is_error = False
+
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = User.objects.create_user(first_name = first_name,
+                                            last_name = last_name,
+                                            email = email,
+                                            username = username,
+                                            password = password)
+
+            message = "el usuario ha sido creado por favor inicie sesion"
+
+        except Exception as e:
+
+            is_error = True
+            message = "error en el sistema por favor comuniquese con soporte"
+            response_data['type_error'] = type(e).__name__
+
+        response_data['message'] = message
+        response_data['is_error'] = is_error
+
+        response_json = json.dumps(response_data)
+        content_type = 'application/json'
+        return HttpResponse(response_json, content_type)
 
 class SignInView(TemplateView):
     template_name = "home/sign-in-form.html"
--- static/js/users/users.js
+++ static/js/users/users.js
@@ -39,3 +39,139 @@ function signIn()
     });
   }
 }
+
+function searchEmail()
+{
+  var getEmail = document.getElementById("email");
+  var validateEmail = getEmail.validate();
+  if(validateEmail)
+  {
+    var email = getEmail.value;
+
+    $.ajax({
+
+      type: 'get',
+      data:{'email': email},
+      url: '/sign-up/',
+      success: function(response)
+      {
+        var isError = response.is_error;
+        var message = response.message;
+
+        if(!isError)
+        {
+          var exist = response.exist;
+          if(exist)
+          {
+            document.getElementById("first_name").readonly = true;
+            document.getElementById("last_name").readonly = true;
+            document.getElementById("password").readonly = true;
+            document.getElementById("again_password").readonly = true;
+            document.getElementById("save_form").disabled = true;
+            document.getElementById("cancel").disabled = true;
+
+
+            alert(message);
+          }else
+          {
+            document.getElementById("first_name").readonly = false
+            document.getElementById("last_name").readonly = false
+            document.getElementById("password").readonly = false
+            document.getElementById("again_password").readonly = false
+            document.getElementById("save_form").disabled = false
+            document.getElementById("cancel").disabled = false
+            alert(message);
+
+            var nameMatch = email.match(/^([^@]*)@/);
+            var name = nameMatch ? nameMatch[1] : null;
+            document.getElementById("username").value = name;
+          }
+        }else
+        {
+          alert(message);
+        }
+      }
+
+    });
+  }
+}
+
+function signUp()
+{
+  var getEmail = document.getElementById("email");
+  var getFirstName = document.getElementById("first_name");
+  var getLastName = document.getElementById("last_name");
+  var getUsername = document.getElementById("username");
+  var getPassword = document.getElementById("password");
+  var getAgainPassword = document.getElementById("again_password");
+
+  var validateEmail = getEmail.validate();
+  var validateFirstName = getFirstName.validate();
+  var validateLastName = getLastName.validate();
+  var validateUsername = getUsername.validate();
+  var validatePassword = getPassword.validate();
+  var validateAgainPassword = getAgainPassword.validate();
+
+  if(validatePassword ==validateAgainPassword)
+  {
+    if(validateEmail && validateFirstName && validateLastName && validateUsername && validatePassword && validateAgainPassword)
+    {
+
+      var email = getEmail.value;
+      var firstName = getFirstName.value;
+      var lastName = getLastName.value;
+      var username = getUsername.value;
+      var password = getPassword.value;
+      var csrftoken = getCookie('csrftoken');
+
+      $.ajax({
+
+        type: 'post',
+        data:{'email':email,
+              'first_name':firstName,
+              'last_name':lastName,
+              'username':username,
+              'password':password,
+              'csrfmiddlewaretoken': csrftoken},
+
+        url: '/sign-up/',
+
+        success: function(response)
+        {
+          var isError = response.is_error;
+          var message = response.message;
+
+          if(!isError)
+          {
+
+            document.getElementById("first_name").readonly = true;
+            document.getElementById("last_name").readonly = true;
+            document.getElementById("password").readonly = true;
+            document.getElementById("again_password").readonly = true;
+            document.getElementById("save_form").disabled = true;
+            document.getElementById("cancel").disabled = true;
+
+            document.getElementById("first_name").value = "";
+            document.getElementById("last_name").value = "";
+            document.getElementById("password").value = "";
+            document.getElementById("again_password").value = "";
+
+            alert(message);
+
+          }else
+          {
+            alert(message);
+          }
+        }
+      });
+
+    }
+
+
+  }else
+  {
+    alert("las contraseñas no coinciden")
+  }
+
+
+}
